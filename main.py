247be0b5fa6471e02ff5491f758a261eeccb86d5
Pablo Balbi <pablo.l.balbi@gmail.com>
2021-04-22 20:57:33 -0300

    parametrized configurations

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 from hashlib import sha1
 from urllib.parse import urlparse
 from dataclasses import dataclass
+from typing import List, TextIO
+import json
+import logging
 
-urls = {
-    "https://www.argenprop.com/departamento-alquiler-barrio-palermo-barrio-belgrano-barrio-colegiales-barrio-villa-crespo-2-ambientes-y-3-ambientes-25000-75000-pesos-1-o-m%c3%a1s-cocheras-desde-45-m2-cubiertos-con-ambiente-balcon-orden-masnuevos"
-}
+logging.basicConfig(level=logging.INFO)
 
 
 @dataclass
@@ -23,20 +24,56 @@ class Parser:
             yield {"id": _id, "url": "{}{}".format(self.website, href)}
 
 
-parsers = [
-    Parser(website="https://www.zonaprop.com.ar", link_regex="a.go-to-posting"),
-    Parser(website="https://www.argenprop.com", link_regex="div.listing__items div.listing__item a"),
-    Parser(website="https://inmuebles.mercadolibre.com.ar", link_regex="li.results-item .rowItem.item a"),
-]
+@dataclass
+class Configuration:
+    seen_file: str
+    parsers: List[Parser]
+    urls: List[str]
+    telegram_key: str
+    telegram_room_id: str
+
+
+configuration: Configuration = None
+
+
+def from_config_file(file_ptr: TextIO):
+    logging.info("Reading raw configuration file")
+    c = json.load(file_ptr)
+
+    app_c = c["app"]
+
+    parsers = [Parser(parser_c["website"], parser_c["link_regex"])
+               for parser_c in app_c["parsers"]]
+    logging.info("Registered %d parsers", len(parsers))
+
+    tlg_c = c["telegram"]
+
+    return Configuration(
+        app_c["seen_file"],
+        parsers,
+        app_c["urls"],
+        tlg_c["key"],
+        tlg_c["room_id"]
+    )
+
+
+def _bootstrap() -> Configuration:
+    logging.info("Opening configuration file")
+    try:
+        with open("Config.json", "r") as f:
+            return from_config_file(f)
+    except Exception as e:
+        logging.fatal("Failed to bootstrap scraper. Cause: %s", e)
+        raise Exception("failed to bootstrap scraper")
 
 
 def _main():
-    for url in urls:
+    for url in configuration.urls:
         res = requests.get(url)
         ads = list(extract_ads(url, res.text))
         seen, unseen = split_seen_and_unseen(ads)
 
-        print("{} seen, {} unseen".format(len(seen), len(unseen)))
+        logging.info("%d seen, %d unseen", len(seen), len(unseen))
 
         for u in unseen:
             # notify(u)
@@ -47,7 +84,8 @@ def _main():
 
 def extract_ads(url, text):
     uri = urlparse(url)
-    parser = next(p for p in parsers if uri.hostname in p.website)
+    parser = next(
+        p for p in configuration.parsers if uri.hostname in p.website)
     return parser.extract_links(text)
 
 
@@ -60,7 +98,7 @@ def split_seen_and_unseen(ads):
 
 def get_history():
     try:
-        with open("seen.txt", "r") as f:
+        with open(configuration.seen_file, "r") as f:
             return {l.rstrip() for l in f.readlines()}
     except:
         return set()
@@ -69,15 +107,21 @@ def get_history():
 def notify(ad):
     bot = "YOUR_BOT_ID"
     room = "YOUR_CHAT_ROOM_ID"
-    url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(bot, room, ad["url"])
+    url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(
+        bot, room, ad["url"])
     r = requests.get(url)
 
 
 def mark_as_seen(unseen):
-    with open("seen.txt", "a+") as f:
+    with open(configuration.seen_file, "a+") as f:
         ids = ["{}\n".format(u["id"]) for u in unseen]
         f.writelines(ids)
 
 
 if __name__ == "__main__":
+    logging.info("Starting scraper")
+
+    configuration = _bootstrap()
+    logging.info("Scraper bootstraped successfully. Running!")
+
     _main()
